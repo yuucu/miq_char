abd80d31dcaa85320bdd1d457112c1ec3d23ae2d
satokibi <td182007@cis.fukuoka-u.ac.jp>
2018-12-11 01:31:40 +0900

    canvasではなく画像を出力するように変更

--- docs/js/myscript.js
+++ docs/js/myscript.js
@@ -2,41 +2,45 @@ window.onload = function() {
 
   var cv = document.getElementById("cv");
   var ctx = cv.getContext('2d');
-  ctx.font = "18px MiQglyph";
+  var font_size = 56;
+  var initial_x = 40;
+  var initial_y = 80;
+  var line_height = 1.1;
+  ctx.font = font_size + "px MiQglyph";
 
   var input = document.getElementById("input");
-  var miq_btn = document.getElementById("miq-btn");
-
-  miq_btn.onclick = function() {
-    clearCanvas(cv, ctx);
-    drawText(ctx, input.value);
-  };
-
 
   input.onkeyup = function() {
     clearCanvas(cv, ctx);
-    drawText(ctx, input.value);
+    drawText(ctx, input.value, font_size, initial_x, initial_y, line_height);
+    toImage(cv);
   };
   input.change = function() {
     clearCanvas(cv, ctx);
-    drawText(ctx, input.value);
+    drawText(ctx, input.value, font_size, initial_x, initial_y, line_height);
+    toImage(cv);
+  };
+
+
+  var save_btn = document.getElementById("save-btn");
+
+  save_btn.onclick = function() {
+    toImage(cv);
+    console.log("aiueo");
   };
 
 };
 
 
-function drawText(ctx, input){
-
+function drawText(ctx, input, font_size, ini_x, ini_y, line_height){
   // full2half
   input_half = input.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
       return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
   });
-
   var lines = input_half.split( "\n" );
   for( var i=0, len=lines.length; i<len; i++ ) {
     var line = lines[i];
-    var addY = 18;
-    ctx.fillText(line, 16+0, 32+22*i);
+    ctx.fillText(line, ini_x+0, ini_y+font_size*i*line_height);
   }
 }
 
@@ -44,3 +48,8 @@ function clearCanvas(cv, ctx) {
   ctx.clearRect(0, 0, cv.width, cv.height);
 }
 
+function toImage(cv) {
+  var png = cv.toDataURL();
+  console.log(png);
+  document.getElementById("new-image").src = png;
+}
